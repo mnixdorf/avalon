700cef626641122565a34e38f4f3877d2b55b404
mnixdorf <35327832+mnixdorf@users.noreply.github.com>
2021-12-11 20:22:09 +0100

    added simple shape and camera movement

--- control/MainMenuControl.java
+++ control/MainMenuControl.java
@@ -1,27 +1,92 @@
 package control;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.*;
 import javafx.scene.control.Button;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.shape.Box;
+import javafx.scene.shape.DrawMode;
+import javafx.scene.transform.Rotate;
+import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
 import model.game.*;
 
-import java.io.IOException;
-import java.net.URL;
-
-public class MainMenuControl {
+public class MainMenuControl{
     @FXML
     private Button btnStart;
+
+    PerspectiveCamera camera;
     @FXML
-    private void launchGame() throws IOException {
-        Stage stage = (Stage) btnStart.getScene().getWindow();
-        System.out.println("Button clicked");
-        URL path = getClass().getResource("/views/Launched.fxml");
-        Parent root = FXMLLoader.load(path);
-        Scene scene = new Scene(root ,800, 600);
-        stage.setScene(scene);
-        stage.show();
+    private void launchGame() throws Exception {
+        //Stage stage = (Stage) btnStart.getScene().getWindow();
+        //System.out.println("Button clicked");
+        //URL path = getClass().getResource("/views/Launched.fxml");
+        //Parent root = FXMLLoader.load(path);
+        //Scene scene = new Scene(root ,800, 600);
+        //stage.setScene(scene);
+        //stage.show();
+
+        Stage primaryStage = (Stage) btnStart.getScene().getWindow();
+        primaryStage.setResizable(false);
+        Scene scene = new Scene(initRectCamera());
+
+        //Key press events on scene
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+
+            }
+        });
+
+        //TODO set rotation with mouse
+        //TODO use animation for camera movement
+
+        scene.setOnKeyReleased(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                switch (event.getCode()) {
+                    case ESCAPE: Platform.exit();
+                        break;
+                    case W:
+                        System.out.println("KEY STROKE RECOGNIZED: w");
+                        camera.setTranslateZ(camera.getTranslateZ() + 1);
+                        break;
+                    case A:
+                        System.out.println("KEY STROKE RECOGNIZED: a");
+                        camera.setTranslateX(camera.getTranslateX() - 1);
+                        break;
+                    case S:
+                        System.out.println("KEY STROKE RECOGNIZED: s");
+                        camera.setTranslateZ(camera.getTranslateZ() - 1);
+                        break;
+                    case D:
+                        System.out.println("KEY STROKE RECOGNIZED: d");
+                        camera.setTranslateX(camera.getTranslateX() + 1);
+                        break;
+                    default:
+                }
+            }
+        });
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public Parent initRectCamera() {
+        Box area = new Box(5, 0, 5);
+        area.setDrawMode(DrawMode.FILL);
+        camera = new PerspectiveCamera(true);
+        camera.getTransforms().addAll (
+                new Rotate(-20, Rotate.X_AXIS),
+                new Translate(0, 0, -15));
+        Group root = new Group();
+        root.getChildren().addAll(camera, area);
+        SubScene subScene = new SubScene(root, 1280,1024);
+        subScene.setCamera(camera);
+        Group group = new Group();
+        group.getChildren().add(subScene);
+        return group;
     }
 }
